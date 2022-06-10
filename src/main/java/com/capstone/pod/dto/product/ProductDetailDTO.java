453e91d1fdfbe1a316915acb0e3736775a84f334
Hung <hungamtp@gmail.com>
2022-06-10 15:22:38 +0700

    get product by id

---
+++ src/main/java/com/capstone/pod/dto/product/ProductDetailDTO.java
@@ -0,0 +1,22 @@
+package com.capstone.pod.dto.product;
+
+import com.capstone.pod.dto.factory.FactoryProductDetailDTO;
+import lombok.*;
+
+import java.util.Collection;
+import java.util.List;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class ProductDetailDTO {
+    private int id;
+    private String name;
+    private String description;
+    private List<String> images;
+    private String categoryName;
+    private List<String> tags;
+    private List<FactoryProductDetailDTO> factories;
+}
