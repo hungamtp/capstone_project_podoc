99af4ac76eb87124dd75572026f56178d7b88839
Sang <vuvietsang10a9@gmail.com>
2022-06-25 16:26:25 +0700

    update getFactoryByCredential Id api

--- src/main/java/com/capstone/pod/dto/sizecolor/SizeColorInFactoryDetailDto.java
+++ src/main/java/com/capstone/pod/dto/sizecolor/SizeColorInFactoryDetailDto.java
@@ -1,12 +1,14 @@
 package com.capstone.pod.dto.sizecolor;
 
 import lombok.Builder;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
 @Builder
 @Getter
 @Setter
+@EqualsAndHashCode
 public class SizeColorInFactoryDetailDto {
     private int quantity;
     private String size;
