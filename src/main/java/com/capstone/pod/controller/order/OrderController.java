ad61a4077d0b2e3f5996836f1e853a48ec130383
Sang <vuvietsang10a9@gmail.com>
2022-06-26 14:33:50 +0700

    update Order api preauth

--- src/main/java/com/capstone/pod/controller/order/OrderController.java
+++ src/main/java/com/capstone/pod/controller/order/OrderController.java
@@ -1,10 +1,12 @@
 package com.capstone.pod.controller.order;
 
 import com.capstone.pod.constant.order.OrderSuccessMessage;
+import com.capstone.pod.constant.role.RolePreAuthorize;
 import com.capstone.pod.dto.http.ResponseDto;
 import com.capstone.pod.services.OrdersService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -16,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class OrderController {
     private final OrdersService ordersService;
     @PostMapping
+    @PreAuthorize(RolePreAuthorize.ROLE_USER)
     public ResponseEntity<ResponseDto> addOrder(@RequestParam int cartId){
         ResponseDto<Void> responseDto = new ResponseDto();
         ordersService.addOrder(cartId);
