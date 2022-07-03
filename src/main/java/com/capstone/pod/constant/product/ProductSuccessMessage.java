5c2a8c9b1d66567016b255ef5c03c6d2c94ad2e9
Sang <vuvietsang10a9@gmail.com>
2022-07-03 14:21:50 +0700

    update product blueprint api

--- src/main/java/com/capstone/pod/constant/product/ProductSuccessMessage.java
+++ src/main/java/com/capstone/pod/constant/product/ProductSuccessMessage.java
@@ -4,7 +4,7 @@ public final class ProductSuccessMessage {
     public static final String ADD_PRODUCT_SUCCESS = "Add product successfully";
     public static final String ADD_PRODUCT_BLUEPRINT_SUCCESS = "Add product blueprint successfully";
     public static final String UPDATE_PRODUCT_SUCCESS = "Update product successfully";
-    public static final String GET_BLUEPRINTS_PRODUCT_SUCCESS = "Get design by product id successfully";
+    public static final String GET_BLUEPRINTS_PRODUCT_SUCCESS = "Get product blueprint by product id successfully";
     public static final String GET_ALL_PRODUCT_SUCCESS = "Get all products successfully";
     public static final String GET_PRODUCT_BY_ID_SUCCESS = "Get product by id successfully";
     public static final String DELETE_PRODUCT_SUCCESS = "Delete product successfully";
