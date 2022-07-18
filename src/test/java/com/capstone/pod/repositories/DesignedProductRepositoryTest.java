a660673fdfa7f9c3c50455be299bc0dcdc6884d5
Sang Vu <vuvietsang10a9@gmail.com>
2022-07-18 20:51:52 +0700

    delete test

--- src/test/java/com/capstone/pod/repositories/DesignedProductRepositoryTest.java
+++ src/test/java/com/capstone/pod/repositories/DesignedProductRepositoryTest.java
@@ -9,17 +9,4 @@ import java.time.LocalDate;
 
 @SpringBootTest
 class DesignedProductRepositoryTest {
-
-    @Autowired
-    private DesignedProductRepository designedProductRepository;
-
-    @Autowired
-    private OrdersRepository ordersRepository;
-
-    @Test
-    public void testGetBestSellerDesignedProduct() {
-       Double a = ordersRepository.getInComeByUserId("1880aacf-9a00-475a-9c34-92881a5acee0" , LocalDate.now().minusYears(1) , LocalDate.now());
-        System.out.println(a);
-    }
-
 }
