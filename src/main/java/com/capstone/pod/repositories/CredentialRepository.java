2d3b13c6b927cb26c2c6e83f1d5e6e6ab391a7ed
Sang Vu <vuvietsang10a9@gmail.com>
2022-08-06 17:11:56 +0700

    update findByEmail

--- src/main/java/com/capstone/pod/repositories/CredentialRepository.java
+++ src/main/java/com/capstone/pod/repositories/CredentialRepository.java
@@ -9,5 +9,6 @@ import java.util.Optional;
 
 public interface CredentialRepository extends JpaRepository<Credential,String> {
         public Optional<Credential> findCredentialByEmail(String email);
+        public Optional<Credential> findCredentialByEmailLikeIgnoreCase(String email);
         public Page<Credential> findAllByRoleName(Pageable pageable, String roleName);
 }
