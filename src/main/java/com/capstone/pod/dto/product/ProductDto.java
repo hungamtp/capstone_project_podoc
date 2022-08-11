cbd7b2e9429ae53438e859359ad3b298f385ee7d
Sang Vu <vuvietsang10a9@gmail.com>
2022-08-11 12:51:00 +0700

    update sort of get product factory doesnot have yet and getfactoryBycredentialId api

--- src/main/java/com/capstone/pod/dto/product/ProductDto.java
+++ src/main/java/com/capstone/pod/dto/product/ProductDto.java
@@ -1,10 +1,9 @@
 package com.capstone.pod.dto.product;
 
-import com.capstone.pod.dto.sizecolor.SizeColorDto;
 import com.capstone.pod.dto.sizecolor.SizeColorInFactoryDetailDto;
 import lombok.*;
 
-import java.util.Collection;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Set;
 
@@ -21,6 +20,7 @@ public class ProductDto {
     private String material;
     private boolean isPublic;
     private boolean isDeleted;
+    private LocalDateTime createDate;
     private List<ProductImagesDto> productImages;
     private String categoryName;
     private Set<SizeColorInFactoryDetailDto> sizeColors;
