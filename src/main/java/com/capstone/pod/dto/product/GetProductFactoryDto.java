274e38f2f459c586a26ab8174ebf78deddefc91e
Sang Vu <vuvietsang10a9@gmail.com>
2022-08-11 20:12:54 +0700

    update sort

--- src/main/java/com/capstone/pod/dto/product/GetProductFactoryDto.java
+++ src/main/java/com/capstone/pod/dto/product/GetProductFactoryDto.java
@@ -4,13 +4,10 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.time.LocalDateTime;
-
 @Builder
 @Getter
 @Setter
 public class GetProductFactoryDto {
     private String id;
     private String name;
-    private LocalDateTime createDate;
 }
--- src/main/java/com/capstone/pod/dto/product/ProductDto.java
+++ src/main/java/com/capstone/pod/dto/product/ProductDto.java
@@ -3,7 +3,6 @@ package com.capstone.pod.dto.product;
 import com.capstone.pod.dto.sizecolor.SizeColorInFactoryDetailDto;
 import lombok.*;
 
-import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Set;
 
@@ -20,7 +19,6 @@ public class ProductDto {
     private String material;
     private boolean isPublic;
     private boolean isDeleted;
-    private LocalDateTime createDate;
     private List<ProductImagesDto> productImages;
     private String categoryName;
     private Set<SizeColorInFactoryDetailDto> sizeColors;
