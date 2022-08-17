70d59fa4bfe44186b0be1dc1379437d74996bd3d
Sang Vu <vuvietsang10a9@gmail.com>
2022-08-17 16:03:46 +0700

    update AddPriceByFactoryDto api

--- src/main/java/com/capstone/pod/dto/factory/AddPriceByFactoryDto.java
+++ src/main/java/com/capstone/pod/dto/factory/AddPriceByFactoryDto.java
@@ -3,6 +3,7 @@ package com.capstone.pod.dto.factory;
 import com.capstone.pod.constant.validation_message.ValidationMessage;
 import lombok.*;
 
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
 @Builder
@@ -13,6 +14,6 @@ import javax.validation.constraints.NotNull;
 public class AddPriceByFactoryDto {
     @NotNull(message = ValidationMessage.PRICE_VALID_MESSAGE)
     private double price;
-    @NotNull(message = ValidationMessage.MATERIAL_VALID_MESSAGE)
+    @NotBlank(message = ValidationMessage.MATERIAL_VALID_MESSAGE)
     private String material;
 }
